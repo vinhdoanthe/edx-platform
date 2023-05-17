83c7dec87d16a8554594804e03641a2e41ab3b3d
Vinh Doan-The <vinhdt@teky.edu.vn>
2023-05-17 21:08:56 +0700

    Comment some blocks

--- lms/djangoapps/landing_page/views.py
+++ lms/djangoapps/landing_page/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from common.djangoapps.edxmako.shortcuts import render_to_response
-
+from openedx.core.djangoapps.user_authn.toggles import should_redirect_to_authn_microfrontend
 
 # Create your views here.
 def index(request):
-    context = {}
+    should_redirect_to_authn_mfe = should_redirect_to_authn_microfrontend()
+
+    context = {
+        'should_redirect_to_authn_mfe': should_redirect_to_authn_mfe,
+    }
     return render(request, 'landing_page/index.html', context=context)
