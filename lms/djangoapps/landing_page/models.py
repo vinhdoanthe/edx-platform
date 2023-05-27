99bff2980babd0c57391c264dc8f2da00eb8d5fc
canhdt <canhdt@teky.vn>
2023-05-28 04:40:15 +0700

    Name: Final template
    Description:
    - Apply final template
    - Mange cms with wagtail

--- lms/djangoapps/landing_page/models.py
+++ lms/djangoapps/landing_page/models.py
@@ -1,13 +1,177 @@
 from django.db import models
 
 from wagtail.models import Page
-from wagtail.fields import RichTextField
+from wagtail.fields import StreamField
+from wagtail.blocks import (
+        RichTextBlock, TextBlock, CharBlock,
+        BooleanBlock, URLBlock, ListBlock,
+        StructBlock, IntegerBlock
+        )
 from wagtail.admin.panels import FieldPanel
+from wagtail.images.blocks import ImageChooserBlock
+
+
+class FeatureItem(StructBlock):
+    icon = ImageChooserBlock()
+    title = CharBlock(max_length=1000)
+    description = TextBlock()
+    link = URLBlock()
+    active = BooleanBlock(default=True, required=False)
+
+
+class MenuItem(StructBlock):
+    title = CharBlock(max_length=1000)
+    enable = BooleanBlock(default=True, required=False)
+
+
+class MissionItem(StructBlock):
+    icon = ImageChooserBlock()
+    title = CharBlock(max_length=1000)
+    description = TextBlock()
+    enable = BooleanBlock(default=True, required=False)
+
+
+class WhyItem(StructBlock):
+    icon = ImageChooserBlock()
+    title = CharBlock(max_length=1000)
+    description = TextBlock()
+    enable = BooleanBlock(default=True, required=False)
+
+
+class ProofPointItem(StructBlock):
+    icon = ImageChooserBlock()
+    number = IntegerBlock(max_length=1000)
+    description = TextBlock()
+    enable = BooleanBlock(default=True, required=False)
+
+
+class CaseStudyItem(StructBlock):
+    title = CharBlock(max_length=1000)
+    description = TextBlock()
+    thumbnail = ImageChooserBlock()
+    video = URLBlock()
+    enable = BooleanBlock(default=True, required=False)
+
+
+class TestimonialItem(StructBlock):
+    quote = TextBlock()
+    user_avatar = ImageChooserBlock()
+    user_name = TextBlock()
+    enable = BooleanBlock(default=True, required=False)
+
+
+class FooterItem(StructBlock):
+    title = CharBlock(max_length=1000)
+    item = ListBlock(
+            StructBlock([
+                ('title', CharBlock(max_length=1000)),
+                ('link', URLBlock()),
+                ])
+            )
+    enable = BooleanBlock(default=True, required=False)
 
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
+    active = models.BooleanField(default=True)
+
+    menu = StreamField([
+        ('logo', ImageChooserBlock()),
+        ('key_selling_points', MenuItem()),
+        ('why', MenuItem()),
+        ('mission', MenuItem()),
+        ('proof_points', MenuItem()),
+        ('services', MenuItem()),
+        ('case_studies', MenuItem()),
+        ('testimonials', MenuItem()),
+        ('subscription_form', MenuItem())
+        ], use_json_field=True, blank=True)
+
+    banner = StreamField([
+        ('title', RichTextBlock()),
+        ('description', TextBlock()),
+        ('image', ImageChooserBlock()),
+        ('button_text', CharBlock(max_length=1000)),
+        ('button_link', URLBlock()),
+        ('enable', BooleanBlock(default=True, required=False))
+        ], use_json_field=True, blank=True)
+
+    features = StreamField([
+        ('list_items', ListBlock(FeatureItem())),
+        ('enable', BooleanBlock(default=True, required=False))
+        ], use_json_field=True, blank=True)
+
+    why = StreamField([
+    #    ('image', ImageChooserBlock()),
+        ('title', CharBlock(max_length=1000)),
+        ('description', TextBlock()),
+        ('list_items', ListBlock(WhyItem())),
+        ('enable', BooleanBlock(default=True, required=False))
+        ], use_json_field=True, blank=True)
+
+    mission = StreamField([
+        ('title', CharBlock(max_length=1000)),
+        ('description', TextBlock()),
+        ('image', ImageChooserBlock()),
+        ('list_items', ListBlock(MissionItem())),
+        ('enable', BooleanBlock(default=True, required=False))
+        ], use_json_field=True, blank=True)
+
+    proof_points = StreamField([
+        ('list_items', ListBlock(ProofPointItem())),
+        ], use_json_field=True, blank=True)
+
+    services = StreamField([
+        ('image', ImageChooserBlock()),
+        ('title', CharBlock(max_length=1000)),
+        ('description', TextBlock()),
+        ('list_items', ListBlock(WhyItem())),
+        ('enable', BooleanBlock(default=True, required=False))
+        ], use_json_field=True, blank=True)
+
+    case_studies = StreamField([
+        ('title', CharBlock(max_length=1000)),
+        ('list_items', ListBlock(CaseStudyItem())),
+        ], use_json_field=True, blank=True)
+
+    testimonials = StreamField([
+        ('title', CharBlock(max_length=1000)),
+        ('description', TextBlock()),
+        ('list_items', ListBlock(TestimonialItem())),
+        ], use_json_field=True, blank=True)
+
+    subscription_form = StreamField([
+        ('title', CharBlock(max_length=1000)),
+        ('description', TextBlock()),
+        ], use_json_field=True, blank=True)
+
+    footer = StreamField([
+        ('description', TextBlock()),
+        ('facebook_contact', StructBlock([
+            ('url', URLBlock()),
+            ('enable', BooleanBlock(default=True, required=False)),
+            ])),
+        ('instagram_contact', StructBlock([
+            ('url', URLBlock()),
+            ('enable', BooleanBlock(default=True, required=False)),
+            ])),
+        ('linkedin_contact', StructBlock([
+            ('url', URLBlock()),
+            ('enable', BooleanBlock(default=True, required=False)),
+            ])),
+        ('list_items', ListBlock(FooterItem())),
+        ('location', TextBlock()),
+        ], use_json_field=True, blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('body'),
-    ]
+            FieldPanel('menu'),
+            FieldPanel('banner'),
+            FieldPanel('features'),
+            FieldPanel('mission'),
+            FieldPanel('why'),
+            FieldPanel('proof_points'),
+            FieldPanel('services'),
+            FieldPanel('case_studies'),
+            FieldPanel('testimonials'),
+            FieldPanel('subscription_form'),
+            FieldPanel('footer'),
+            ]
--- lms/djangoapps/landing_page/views.py
+++ lms/djangoapps/landing_page/views.py
@@ -1,8 +1,63 @@
 from django.shortcuts import render
-from common.djangoapps.edxmako.shortcuts import render_to_response
+from openedx.core.djangoapps.user_authn.toggles import (
+        should_redirect_to_authn_microfrontend
+        )
 
+from .models import HomePage
+from .services import (
+        get_stream_field_data,
+        list_to_dict
+)
+
+from wagtail.images.views.serve import generate_image_url
 
 # Create your views here.
+
+
 def index(request):
-    context = {}
+    should_redirect_to_authn_mfe = should_redirect_to_authn_microfrontend()
+
+    home = HomePage.objects.first()
+
+    context = {
+        'should_redirect_to_authn_mfe': should_redirect_to_authn_mfe,
+        'missing_home': False
+    }
+
+    if not home:
+        context['missing_home'] = True
+
+        return render(request, 'landing_page/index.html', context=context)
+
+    menu = get_stream_field_data(home, 'menu')
+    banner = get_stream_field_data(home, 'banner')
+    features = get_stream_field_data(home, 'features')
+    why = get_stream_field_data(home, 'why')
+    mission = get_stream_field_data(home, 'mission')
+    proof_points = get_stream_field_data(home, 'proof_points')
+    services = get_stream_field_data(home, 'services')
+    case_studies = get_stream_field_data(home, 'case_studies')
+    testimonials = get_stream_field_data(home, 'testimonials')
+    subscription_form = get_stream_field_data(home, 'subscription_form')
+    footer = get_stream_field_data(home, 'footer')
+    # mission_image = generate_image_url(mission[0]['image'].id, 'fill-1920x1080')
+    mission_image = ''
+
+    context.update({
+        'homepage': home,
+        'menu': menu,
+        'banner': list_to_dict(banner),
+        'features': list_to_dict(features),
+        'why': list_to_dict(why),
+        'mission': list_to_dict(mission),
+        'proof_points': list_to_dict(proof_points),
+        'services': list_to_dict(services),
+        'case_studies': list_to_dict(case_studies),
+        'testimonials': list_to_dict(testimonials),
+        'subscription_form': list_to_dict(subscription_form),
+        'footer': list_to_dict(footer),
+        'mission_image': mission_image,
+        'missing_home': False
+        })
+
     return render(request, 'landing_page/index.html', context=context)
